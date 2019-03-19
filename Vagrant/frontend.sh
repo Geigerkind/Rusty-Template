01b250fb075ca99703333926abf5474ab740e64a
Tom <tomdymel@web.de>
2019-03-19 23:01:53 +0100

    Just using workbox sw, that one works at least

--- Vagrant/frontend.sh
+++ Vagrant/frontend.sh
@@ -1,12 +1,8 @@
 # Creating www directory
 mkdir -p /var/www;
 
-# Debug: Hello World
-mkdir /var/www/jaylapp;
-echo "Hello World!" > /var/www/jaylapp/index.html;
-
 # Giving it user accessible rights
 chmod -R 777 /var/www;
 
 # Compiling the frontend
-# bash /me/Frontend/compile.sh debug false;
+bash /me/Frontend/compile.sh debug false;
