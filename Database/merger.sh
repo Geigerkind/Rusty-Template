5274ec9a5c675614e2901e1a717579e727826f77
Tom <tomdymel@web.de>
2019-10-26 15:39:54 +0000

    Merger check uses absolute path now

--- Database/merger.sh
+++ Database/merger.sh
@@ -1,11 +1,11 @@
-if [ ! -f "./db_patch_count" ]; then
+if [ ! -f "db_patch_count" ]; then
   echo "-1" > ./db_patch_count
 fi
 COUNT=$(cat ./db_patch_count)
 COUNT=$(expr ${COUNT} + 0)
 
-if [ -f "./merge.sql" ]; then
-  rm merge.sql
+if [ -f "merge.sql" ]; then
+  rm ./merge.sql
 fi
 for filename in ./*.sql; do
   if [ ! -f "${filename}" ]; then
