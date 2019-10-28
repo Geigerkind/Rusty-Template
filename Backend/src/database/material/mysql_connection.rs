ef80dda63a34d2cb6e5ea8826af17f6440161dbf
Tom <tomdymel@web.de>
2019-10-28 22:19:05 +0000

    Test

--- Backend/src/database/material/mysql_connection.rs
+++ Backend/src/database/material/mysql_connection.rs
@@ -5,7 +5,7 @@ pub struct MySQLConnection {
 impl MySQLConnection {
   pub fn new(db_name: &str) -> Self
   {
-    let mut dns: String = "mysql://root:vagrant@127.0.0.1:5555/".to_owned();
+    let mut dns: String = "mysql://root:vagrant@127.0.0.1/".to_owned();
     dns.push_str(db_name);
     MySQLConnection {
       con: mysql::Pool::new(dns).unwrap()
