f88dc8f7793aa293bd029e1f096d84be1284d3ba
Tom <tomdymel@web.de>
2019-11-01 17:26:39 +0000

    Adding test to check if invalidation works

--- Backend/src/account/tests/validator.rs
+++ Backend/src/account/tests/validator.rs
@@ -6,6 +6,10 @@ mod tests {
   use crate::account::tools::create::Create;
   use crate::account::service::create::PostCreateMember;
   use crate::database::tools::mysql::execute::Execute;
+  use crate::account::tools::update::Update;
+  use crate::account::service::update::PostChangeStr;
+  use crate::account::tools::login::Login;
+  use crate::account::service::login::PostLogin;
 
   // Helper functions are tested indirectly through all other functions
 
@@ -33,5 +37,35 @@ mod tests {
     };
 
     assert!(!account.validate(&val_pair));
+  }
+
+  #[test]
+  fn validation_invalid_after_update() {
+    let account = Account::default();
+    let post_obj = PostCreateMember {
+      nickname: "klsdkfsowerf".to_string(),
+      mail: "klsdkfsowerf@jaylappTest.dev".to_string(),
+      password: "Password123456Password123456Password123456".to_string()
+    };
+
+    // First login
+    let val_pair = account.create(&post_obj).unwrap();
+    let val_pair2 = account.login(&PostLogin {
+      mail: post_obj.mail,
+      password: post_obj.password
+    }).unwrap();
+
+    assert!(account.validate(&val_pair));
+    assert!(account.validate(&val_pair2));
+
+    let val_pair3 = account.change_password(&PostChangeStr {
+      content: "SuperDuperSecretPasswordDefNotSecretTho".to_string(),
+      validation: val_pair
+    }).unwrap();
+
+    assert!(!account.validate(&val_pair2));
+    assert!(account.validate(&val_pair3));
+
+    account.db_main.execute("DELETE FROM member WHERE mail='klsdkfsowerf@jaylappTest.dev'");
   }
 }
