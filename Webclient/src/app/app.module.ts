2bec1f21d1949c97fb4525a59d6b64537b3b2f51
Tom <tomdymel@web.de>
2019-11-01 23:26:08 +0000

    First draft of cookie consent banner

--- Webclient/src/app/app.module.ts
+++ Webclient/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { CookieBanner } from './cookie_banner/cookie_banner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CookieBanner
   ],
   imports: [
     BrowserModule,
---
+++ Webclient/src/app/cookie_banner/cookie_banner.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "CookieBanner",
+  templateUrl: "./cookie_banner.html",
+  styleUrls: ["./cookie_banner.scss"]
+})
+export class CookieBanner {
+
+}
