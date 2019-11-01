d4cf080f22f30e77ad63bdca429cf8572b940490
Tom <tomdymel@web.de>
2019-11-01 20:06:11 +0000

    Testing validator sub crate

--- Backend/src/util/validator/tests/valid.rs
+++ Backend/src/util/validator/tests/valid.rs
@@ -20,4 +20,27 @@ mod tests {
     assert!(valid::password(pass).is_ok());
   }
 
+  #[test]
+  fn invalid_mail() {
+    let mail = "Test@bla";
+    assert!(!valid::mail(mail));
+  }
+
+  #[test]
+  fn valid_mail() {
+    let mail = "Test.Test@bla.de";
+    assert!(valid::mail(mail));
+  }
+
+  #[test]
+  fn invalid_nickname() {
+    let nickname = "NickName NickName";
+    assert!(!valid::nickname(nickname));
+  }
+
+  #[test]
+  fn valid_nickname() {
+    let nickname = "NickName";
+    assert!(valid::nickname(nickname));
+  }
 }
