c36e3a938356bd8b7e2b7e16a6ae3e50b48bf50f
Tom <tomdymel@web.de>
2019-11-01 20:21:32 +0000

    Refactoring mysql exists

--- Backend/src/database/tools/mysql/exists.rs
+++ Backend/src/database/tools/mysql/exists.rs
@@ -11,23 +11,11 @@ pub trait Exists {
 impl Exists for MySQLConnection {
   fn exists(&self, query_str: &str) -> bool
   {
-    let mut exists: String = "SELECT EXISTS(".to_owned();
-    exists.push_str(query_str);
-    exists.push_str(")");
-    self.select_value(&exists, &|row|{
-      let res: bool = mysql::from_row(row);
-      res
-    }).unwrap()
+    self.select_value(&["SELECT EXISTS(", query_str, ")"].concat(), &|row| { mysql::from_row(row) }).unwrap()
   }
 
   fn exists_wparams(&self, query_str: &str, params: std::vec::Vec<(std::string::String, mysql::Value)>) -> bool
   {
-    let mut exists: String = "SELECT EXISTS(".to_owned();
-    exists.push_str(query_str);
-    exists.push_str(")");
-    self.select_wparams_value(&exists, &|row|{
-      let res: bool = mysql::from_row(row);
-      res
-    }, params).unwrap()
+    self.select_wparams_value(&["SELECT EXISTS(", query_str, ")"].concat(), &|row| { mysql::from_row(row) }, params).unwrap()
   }
 }
