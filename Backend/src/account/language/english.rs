e5be64d2aae5be3faea806edb0235dc831e6a2a7
Tom <tomdymel@web.de>
2019-11-02 17:52:40 +0000

    Backend localization

--- Backend/src/account/language/english.rs
+++ Backend/src/account/language/english.rs
@@ -8,34 +8,32 @@ pub fn init(dictionary: &Dictionary) {
 
   dictionary.register("general.service.success", Language::English, "Success!");
 
-  dictionary.register("general.error.validate", Language::English, "TODO");
-  dictionary.register("general.error.invalid.mail", Language::English, "TODO");
-  dictionary.register("general.error.invalid.nickname", Language::English, "TODO");
-  dictionary.register("general.error.invalid.password", Language::English, "TODO");
-  dictionary.register("general.error.mail_send", Language::English, "TODO");
-  dictionary.register("general.error.unknown", Language::English, "TODO");
+  dictionary.register("general.error.validate", Language::English, "Invalid credentials!");
+  dictionary.register("general.error.invalid.mail", Language::English, "Invalid mail!");
+  dictionary.register("general.error.invalid.nickname", Language::English, "Invalid nickname!");
+  dictionary.register("general.error.mail_send", Language::English, "Mail could not be send!");
+  dictionary.register("general.error.unknown", Language::English, "An unknown error occurred!");
 
-  dictionary.register("create.confirmation.subject", Language::English, "TODO");
-  dictionary.register("create.confirmation.text", Language::English, "TODO: Heartwarming welcome text\nhttps://jaylapp.dev/API/account/confirm/{0}");
-  dictionary.register("create.error.taken.mail", Language::English, "TODO");
-  dictionary.register("create.error.taken.nickname", Language::English, "TODO");
+  dictionary.register("create.confirmation.subject", Language::English, "Confirm your account!");
+  dictionary.register("create.confirmation.text", Language::English, "Greetings!\n\nPlease finish the registration process by clicking on the provided url.\n\nhttps://jaylapp.dev/API/account/confirm/{0}\n\nCheers!");
+  dictionary.register("create.error.taken.mail", Language::English, "This mail is already being used!");
+  dictionary.register("create.error.taken.nickname", Language::English, "This nickname is already being used!");
 
-  dictionary.register("delete.confirmation.subject", Language::English, "TODO");
-  dictionary.register("delete.confirmation.text", Language::English, "TODO: FANCY TEXT\nhttps://jaylapp.dev/API/account/delete/confirm/{0}");
+  dictionary.register("forgot.confirmation.subject", Language::English, "Have you forgotten your password?");
+  dictionary.register("forgot.confirmation.text", Language::English, "Greetings!\nPlease click on the provided url in order to generate a new password.\n\nhttps://jaylapp.dev/API/account/forgot/confirm/{0}\n\nCheers!");
+  dictionary.register("forgot.information.subject", Language::English, "Your new password!");
+  dictionary.register("forgot.information.text", Language::English, "Greetings\n\nThis is your new Password: {0}\n\nPlease change it immediately!\n\nCheers!");
+  dictionary.register("forgot.error.no_forgot_issued", Language::English, "The provided url is invalid!");
 
-  dictionary.register("forgot.confirmation.subject", Language::English, "TODO");
-  dictionary.register("forgot.confirmation.text", Language::English, "TODO: FANCY TEXT\nhttps://jaylapp.dev/API/account/forgot/confirm/{0}");
-  dictionary.register("forgot.information.subject", Language::English, "TODO");
-  dictionary.register("forgot.information.text", Language::English, "TODO: Text\n New Password: ");
-  dictionary.register("forgot.error.no_forgot_issued", Language::English, "TODO");
+  dictionary.register("get.error.nomember", Language::English, "There is no member with this id!");
 
-  dictionary.register("get.error.nomember", Language::English, "TODO");
+  dictionary.register("login.error.credentials", Language::English, "Invalid credentials!");
 
-  dictionary.register("login.error.credentials", Language::English, "TODO");
+  dictionary.register("delete.error.no_delete_issued", Language::English, "A deletion has not been requested for this token!");
+  dictionary.register("delete.error.user_not_removable", Language::English, "Unable to delete user!");
+  dictionary.register("delete.confirmation.subject", Language::English, "Confirm the deletion of your account!");
+  dictionary.register("delete.confirmation.text", Language::English, "Greetings!\n\nPlease confirm the deletion of your account by clicking on the provided url.\n\nhttps://jaylapp.dev/API/account/delete/confirm/{0}\n\nCheers!");
 
-  dictionary.register("delete.error.no_delete_issued", Language::English, "TODO");
-  dictionary.register("delete.error.user_not_removable", Language::English, "TODO");
-
-  dictionary.register("update.error.name_taken", Language::English, "TODO");
-  dictionary.register("update.error.mail_taken", Language::English, "TODO");
+  dictionary.register("update.error.name_taken", Language::English, "This nickname is already taken!");
+  dictionary.register("update.error.mail_taken", Language::English, "This mail is already taken");
 }
