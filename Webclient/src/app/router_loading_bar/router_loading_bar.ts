26eb89da7eb58bd673309988879a04ff6820b931
Tom <tomdymel@web.de>
2019-11-15 19:02:57 +0000

    Routing loading animation

---
+++ Webclient/src/app/router_loading_bar/router_loading_bar.ts
@@ -0,0 +1,40 @@
+import { Component } from "@angular/core";
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+
+@Component({
+  selector: "RouterLoadingBar",
+  templateUrl: "./router_loading_bar.html",
+  styleUrls: ["./router_loading_bar.scss"]
+})
+export class RouterLoadingBar {
+  scaleX: number = 0;
+  displayBar: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      switch(true) {
+        case event instanceof NavigationStart: {
+          this.scaleX = 0;
+          this.displayBar = true;
+
+          const max_iterations = 20;
+          for (let i=0; i<max_iterations; ++i) {
+            const scaleFactor = (0.8/max_iterations) * i;
+            const afterTime = (1000/max_iterations) * i;
+            setTimeout(() => { if (this.scaleX < scaleFactor) this.scaleX = scaleFactor }, afterTime);
+          }
+          break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.scaleX = 1;
+          setTimeout(() => this.displayBar = false, 100);
+          break;
+        }
+        default:
+          break;
+      }
+    });
+  }
+}
