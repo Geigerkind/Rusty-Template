cf1fda6c55cab663bf565fa2a5e9ef9d3a5e2fa8
Tom <tomdymel@web.de>
2019-11-17 18:53:01 +0000

    Styling for reset password component

--- Webclient/src/app/reset_password/reset_password.module.ts
+++ Webclient/src/app/reset_password/reset_password.module.ts
@@ -3,13 +3,17 @@ import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from "@angular/common";
 import { ResetPasswordRoutingModule } from "./routing.module";
 import { ResetPassword } from "./reset_password";
+import { GeneralInputModule } from "../template/general_input/general_input.module";
+import { ConfirmButtonModule } from "../template/confirm_button/confirm_button.module";
 
 @NgModule({
   declarations: [ResetPassword],
   imports: [
     CommonModule,
     TranslateModule,
-    ResetPasswordRoutingModule
+    ResetPasswordRoutingModule,
+    GeneralInputModule,
+    ConfirmButtonModule
   ],
   exports: [ResetPassword]
 })
