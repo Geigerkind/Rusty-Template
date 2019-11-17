c637fa6897aa224e9a99fc7aa5289dde163048a0
Tom <tomdymel@web.de>
2019-11-17 18:33:58 +0000

    login component styling

--- Webclient/src/app/login/login.module.ts
+++ Webclient/src/app/login/login.module.ts
@@ -3,13 +3,19 @@ import { TranslateModule } from "@ngx-translate/core";
 import { CommonModule } from "@angular/common";
 import { LoginRoutingModule } from "./routing.module";
 import { Login } from "./login";
+import { GeneralInputModule } from "../template/general_input/general_input.module";
+import { PasswordInputModule } from "../template/password_input/password_input.module";
+import { ConfirmButtonModule } from "../template/confirm_button/confirm_button.module";
 
 @NgModule({
   declarations: [Login],
   imports: [
     CommonModule,
     TranslateModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    GeneralInputModule,
+    PasswordInputModule,
+    ConfirmButtonModule
   ],
   exports: [Login]
 })
