11923fc579bb7f364de509c3000a0619ef06aeae
Tom <tomdymel@web.de>
2019-11-24 13:52:39 +0000

    Renaming service to transfer in account

--- Backend/src/account/mod.rs
+++ Backend/src/account/mod.rs
@@ -1,6 +1,6 @@
 pub mod domain_value;
 pub mod material;
-pub mod service;
+pub mod transfer;
 pub mod tools;
 pub mod language;
 pub mod tests;
--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -45,12 +45,12 @@ fn main() {
   igniter = igniter.manage(backend.account);
   igniter = igniter.mount("/API/", routes![api_overview]);
   igniter = igniter.mount("/API/account/", routes![
-    account::service::api::api,
-    account::service::token::create_token, account::service::token::get_tokens, account::service::token::delete_token,
-    account::service::delete::request, account::service::delete::confirm,
-    account::service::create::create, account::service::create::confirm, account::service::create::resend_confirm,
-    account::service::get::get,
-    account::service::forgot::receive_confirmation, account::service::forgot::send_confirmation,
-    account::service::update::mail, account::service::update::password, account::service::update::nickname]);
+    account::transfer::api::api,
+    account::transfer::token::create_token, account::transfer::token::get_tokens, account::transfer::token::delete_token,
+    account::transfer::delete::request, account::transfer::delete::confirm,
+    account::transfer::create::create, account::transfer::create::confirm, account::transfer::create::resend_confirm,
+    account::transfer::get::get,
+    account::transfer::forgot::receive_confirmation, account::transfer::forgot::send_confirmation,
+    account::transfer::update::mail, account::transfer::update::password, account::transfer::update::nickname]);
   igniter.launch();
 }
