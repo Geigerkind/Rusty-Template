d4cfca6d6b2c01629702b06c9bf6f6850d2db836
Tom <tomdymel@web.de>
2019-12-04 23:14:01 +0000

    Fixing account creation, adding salt, also fixing account startup

--- Backend/src/account/tools/create.rs
+++ Backend/src/account/tools/create.rs
@@ -49,10 +49,12 @@ impl Create for Account {
       let salt: String = random::alphanumeric(16);
       let pass: String = sha3::hash(&[password, &salt]);
 
-      if self.db_main.execute_wparams("INSERT IGNORE INTO member (`mail`, `password`, `nickname`, `joined`) VALUES (:mail, :pass, :nickname, UNIX_TIMESTAMP())", params!(
+      if self.db_main.execute_wparams("INSERT IGNORE INTO member (`mail`, `password`, `nickname`, `salt`, `joined`) VALUES (:mail, :pass, :nickname, :salt, UNIX_TIMESTAMP())", params!(
       "nickname" => nickname.clone(),
       "mail" => lower_mail.clone(),
-      "pass" => pass.clone()),
+      "pass" => pass.clone(),
+      "salt" => salt.clone()
+      ),
       ) {
         member_id = self.db_main.select_wparams_value("SELECT id FROM member WHERE mail = :mail", &|mut row| {
           row.take(0).unwrap()
