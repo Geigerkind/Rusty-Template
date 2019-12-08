c6007b4bf2616c8ff7127c16ecf8ed94406c5d15
Tom <tomdymel@web.de>
2019-12-08 12:49:13 +0000

    Implementing a request limit

--- Backend/sub_crates/str_util/src/lib.rs
+++ Backend/sub_crates/str_util/src/lib.rs
@@ -1,10 +1,12 @@
 extern crate rand;
 extern crate rand_distr;
 extern crate sha3 as sha;
+extern crate bcrypt as bc;
 
 mod tests;
 mod tools;
 
 pub use self::tools::random;
 pub use self::tools::sha3;
+pub use self::tools::bcrypt;
 pub use self::tools::strformat;
