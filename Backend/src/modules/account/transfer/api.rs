efa2a56f1b5245ea6a2705f8f280facaadf33223
Tom <tomdymel@web.de>
2019-12-14 17:19:19 +0000

    Account api expose changes

--- Backend/src/modules/account/transfer/api.rs
+++ Backend/src/modules/account/transfer/api.rs
@@ -19,16 +19,16 @@ pub fn api() -> Json<Vec<serde_json::Value>> {
 
     // Create
     expose_api_fn("/create/<create_member>", "post", false, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(CreateMember)),
-    expose_api_fn("/create/confirm/<confirmation_id>", "get", false, "application/json", schema_for!(bool), schema_for!(String)),
-    expose_api_fn("/create/resend/", "get", true, "application/json", schema_for!(bool), schema_for!(Nothing)),
+    expose_api_fn("/create/<confirmation_id>", "get", false, "application/json", schema_for!(Result<(), Failure>), schema_for!(String)),
+    expose_api_fn("/create/resend", "get", true, "application/json", schema_for!(Result<(), Failure>), schema_for!(Nothing)),
 
     // Delete
-    expose_api_fn("/delete/confirm/<delete_id>", "get", false, "text/html", schema_for!(Result<(), Failure>), schema_for!(String)),
-    expose_api_fn("/delete/request/", "delete", true, "application/json", schema_for!(Result<(), Failure>), schema_for!(Nothing)),
+    expose_api_fn("/delete/<delete_id>", "get", false, "text/plain", schema_for!(Result<(), Failure>), schema_for!(String)),
+    expose_api_fn("/delete", "delete", true, "application/json", schema_for!(Result<(), Failure>), schema_for!(Nothing)),
 
     // Forgot
-    expose_api_fn("/forgot/confirm/<confirmation_id>", "get", false, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(String)),
-    expose_api_fn("/forgot/send/<mail>", "get", false, "text/html", schema_for!(Result<(), Failure>), schema_for!(String)),
+    expose_api_fn("/forgot/<confirmation_id>", "get", false, "text/plain", schema_for!(Result<APIToken, Failure>), schema_for!(String)),
+    expose_api_fn("/forgot/<mail>", "post", false, "application/json", schema_for!(Result<(), Failure>), schema_for!(String)),
 
     // Login
     expose_api_fn("/login/<credentials>", "post", false, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(Credentials)),
@@ -36,13 +36,13 @@ pub fn api() -> Json<Vec<serde_json::Value>> {
     // Update
     expose_api_fn("/update/password/<password>", "post", true, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(String)),
     expose_api_fn("/update/nickname/<nickname>", "post", true, "application/json", schema_for!(Result<AccountInformation, Failure>), schema_for!(String)),
-    expose_api_fn("/update/mail/request/<mail>", "post", true, "application/json", schema_for!(Result<bool, Failure>), schema_for!(String)),
+    expose_api_fn("/update/mail/<mail>", "post", true, "application/json", schema_for!(Result<bool, Failure>), schema_for!(String)),
     expose_api_fn("/update/mail/<confirmation_id>", "get", false, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(String)),
 
     // Token
-    expose_api_fn("/token/create/<create_token>", "post", true, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(CreateToken)),
-    expose_api_fn("/token/get/", "get", true, "application/json", schema_for!(Result<Vec<APIToken>, Failure>), schema_for!(Nothing)),
-    expose_api_fn("/token/delete/<token_id>", "post", true, "application/json", schema_for!(Result<(), Failure>), schema_for!(u32)),
-    expose_api_fn("/token/update/<prolong_token>", "post", true, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(ProlongToken)),
+    expose_api_fn("/token/<create_token>", "post", true, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(CreateToken)),
+    expose_api_fn("/token", "get", true, "application/json", schema_for!(Result<Vec<APIToken>, Failure>), schema_for!(Nothing)),
+    expose_api_fn("/token/<token_id>", "post", true, "application/json", schema_for!(Result<(), Failure>), schema_for!(u32)),
+    expose_api_fn("/token/prolong/<prolong_token>", "post", true, "application/json", schema_for!(Result<APIToken, Failure>), schema_for!(ProlongToken)),
   ])
 }
