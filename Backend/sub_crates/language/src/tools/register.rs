55ebce62329253cd8f5a52ae7d38b0585c08196c
Tom <tomdymel@web.de>
2019-12-14 13:05:47 +0000

    Backend translation can use env variables now

--- Backend/sub_crates/language/src/tools/register.rs
+++ Backend/sub_crates/language/src/tools/register.rs
@@ -1,3 +1,6 @@
+extern crate dotenv;
+use self::dotenv::dotenv;
+
 use crate::domain_value::Language;
 use crate::material::Dictionary;
 
@@ -9,7 +12,12 @@ impl Register for Dictionary {
   fn register(&self, key: &str, language: Language, value: &str) {
     let mut lang_table = self.table.write().unwrap();
     let key_str: String = String::from(key);
-    let value_str: String = String::from(value);
+    let mut value_str: String = String::from(value);
+    // Replacing environmental variables
+    dotenv().ok();
+    for (key, val) in dotenv::vars() {
+      value_str = value_str.replace(&format!("{{{env_key}}}", env_key=key), &val);
+    }
     let lang_index = language as usize;
     match lang_table.get_mut(&key_str) {
       Some(vec) => {
@@ -21,19 +29,19 @@ impl Register for Dictionary {
             } else {
               panic!("{} is overwritten for the language {} with the content {}!", key, lang_index, value);
             }
-          },
+          }
           // This means that the vector has not been extended to this language yet
           None => {
-            for _ in {vec_length..lang_index} {
+            for _ in { vec_length..lang_index } {
               vec.push(String::new());
             }
             vec.push(value_str);
           }
         }
-      },
+      }
       None => {
         let mut container: Vec<String> = Vec::new();
-        for _ in {0..lang_index} {
+        for _ in { 0..lang_index } {
           container.push(String::new());
         }
         container.push(value_str);
